4b69eb3f5d13e0b5be276061166b55cc600cfaba
Weston Anderson <wanderson2015@kellogg.northwestern.edu>
2015-03-10 01:43:40 -0500

    passing foodid in rating param

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,52 +1,23 @@
 class RatingsController < ApplicationController
   skip_before_action :require_user
 
-
-# Need to define the variable is in the controller, this method is called the action
-# The index is a method in a controller mapping the db to the views 
-  def index 
-    @ratings = Rating.all
-  end
-
-  def show 
-  end 
-  
   def new
+    # get my food
+    @food = Food.find_by(id: params["food_id"])
     @rating = Rating.new
-    @food = Food.find_by(id: params["movie_id"]
-    # Define the food as the what you clicked on 
-    @menus = Menu.where(food_id: params["id"])
-    # Now give me all the restaurants that have it
-    @spots = @menus.restaurants
   end
 
   def create
-
-  #  Rating.create(category: params["category"], description: params["description"])
+    @food = Food.find_by(id: params["movie_id"])
     rating_params = params.require(:rating).permit!
-    @rating = Rating.create(rating_params)
-    if @rating.valid?
-      redirect_to ratings_path, notice: "Cool Dude"
+    @rating = @food.ratings.new(rating_params)
+    @rating.user = current_user
+    if @rating.save
+      redirect_to @food, notice: "Thanks for rating!"
     else
       render "new"
     end
   end
 
-  def edit
-    @ratings = Rating.find_by(id: params["id"])
-  end
-
-  def update
-    rating_params = params.require(:ratings).permit!
-    @ratings = Rating.find_by(id: params["id"])
-    @ratings.update(rating_params)
-    redirect_to ratings_path
-  end
-
-  def destroy
-    @rating = Rating.find_by(id: params["id"])
-    @rating.destroy
-    redirect_to ratings_path
-  end
 
 end
